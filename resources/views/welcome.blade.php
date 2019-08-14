4544d22a27b60b959605d50ef77b0d8e92ad7fc6
marquizq <michaelmallya@gmail.com>
2019-08-14 17:22:33 +0300

    CHanged Welcome to Wednesday

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Wednesday
                 </div>
 
                 <div class="links">
